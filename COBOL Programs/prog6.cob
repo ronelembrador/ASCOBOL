804d2fb9367c7420af0c957aa2ded508c672cb01
Ronel <omembrador@student.apc.edu.ph>
2017-01-27 11:08:53 +0800

    prog6 added

---
+++ COBOL Programs/prog6.cob
@@ -0,0 +1,29 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MovePractice.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  StudentName.
+           02  FirstName   PIC X(8) VALUE "JOHN".
+           02  Surname   PIC X(8) VALUE "POWERS".
+       PROCEDURE DIVISION.
+       Begin.
+           DISPLAY StudentName.
+           MOVE "COUGHLAN" TO Surname.
+           DISPLAY StudentName.
+           MOVE "TOM" TO FirstName.
+           DISPLAY StudentName.
+           MOVE "FITZPATRICK" TO Surname.
+           DISPLAY StudentName.
+           MOVE "ALEXANDER" TO FirstName.
+           DISPLAY StudentName.
+           MOVE "JOHN    RYAN" TO StudentName.
+           DISPLAY StudentName.
+           MOVE ALL "x" TO StudentName.
+           STOP RUN.
+       END PROGRAM MovePractice.
