8eddc35c1b58314568864d73e98a7c6dd22e8800
ronelembrador <omembrador@student.apc.edu.ph>
2017-03-21 10:56:15 +0800

    View page layout fixed

--- WEBPROG/exercises/codeigniter/application/views/news/view.php
+++ WEBPROG/exercises/codeigniter/application/views/news/view.php
@@ -1,10 +1,10 @@
 <?php
 echo '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
-echo $news_item['name'];
-echo $news_item['nickname'];
-echo $news_item['email'];
-echo $news_item['address'];
-echo $news_item['gender'];
-echo $news_item['cellno'];
-echo $news_item['comments']; ?>
+echo '<p>'.$news_item['text'].'</p>';
+echo '<p>Name: '.$news_item['name'].'</p>';
+echo '<p>Nickname: '.$news_item['nickname'].'</p>';
+echo '<p>E-mail: '.$news_item['email'].'</p>';
+echo '<p>Address: '.$news_item['address'].'</p>';
+echo '<p>Gender: '.$news_item['gender'].'</p>';
+echo '<p>Cellphone Number: '.$news_item['cellno'].'</p>';
+echo '<p>Comments: '.$news_item['comments'].'</p>'; ?>
