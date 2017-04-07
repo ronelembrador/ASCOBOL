1b52f02413915025fd07f3912bae423a8ae2da5a
ronelembrador <omembrador@student.apc.edu.ph>
2017-04-07 11:24:15 +0800

    View jumbotron for title and content Create form look changed to bootstrap version and put placeholders and success and error indicators

--- WEBPROG/exercises/codeigniter2/application/views/news/create.php
+++ WEBPROG/exercises/codeigniter2/application/views/news/create.php
@@ -4,45 +4,45 @@
  
 <?php echo form_open('news/create'); ?>    
     <table>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="title">Title</label></td>
-            <td><input type="input" name="title" size="50" /></td>
+            <td><input type="input" class="form-control" name="title" size="50" placeholder="Title of post" /></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="text">Text</label></td>
-            <td><textarea name="text" rows="10" cols="40"></textarea></td>
+            <td><textarea name="text" class="form-control" rows="10" cols="40" placeholder="Description of post"></textarea></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="name">Name</label></td>
-            <td><input type="input" name="name" size="50" /></td>
+            <td><input type="input" class="form-control" name="name" size="50" placeholder="Alphabetical and numeric characters" /></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="nickname">Nickname</label></td>
-            <td><input type="input" name="nickname" size="30"></textarea></td>
+            <td><input type="input" class="form-control" name="nickname" size="30" placeholder="Alphabetical characters"></textarea></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="email">Email</label></td>
-            <td><input type="input" name="email" size="50" /></td>
+            <td><input type="input" class="form-control" name="email" size="50" placeholder="username@email.com" /></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="address">Address</label></td>
-            <td><textarea name="address" rows="10" cols="40"></textarea></td>
+            <td><textarea name="address" class="form-control" rows="10" cols="40" placeholder="Address here. Not required"></textarea></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="gender">Gender</label></td>
-            <td><input type="input" name="gender" size="10"></textarea></td>
+            <td><input type="input" class="form-control" name="gender" size="10" placeholder="M/F/Male/Female"></textarea></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="cellno">Cellphone Number</label></td>
-            <td><input type="input" name="cellno" size="20" /></td>
+            <td><input type="input" class="form-control" name="cellno" size="20" placeholder="11 number cell number here." /></td>
         </tr>
-        <tr>
+        <tr class = "has-success has-error">
             <td><label for="comments">Comments</label></td>
-            <td><textarea name="comments" rows="10" cols="40"></textarea></td>
+            <td><textarea name="comments" class="form-control" rows="10" cols="40" placeholder="Any comments here. Not required"></textarea></td>
         </tr>
         <tr>
             <td></td>
-            <td><input type="submit" name="submit" value="Create news item" /></td>
+            <td><input type="submit" class="form-control" name="submit" value="Create news item" /></td>
         </tr>
     </table>    
 </form>
--- WEBPROG/exercises/codeigniter2/application/views/news/view.php
+++ WEBPROG/exercises/codeigniter2/application/views/news/view.php
@@ -1,10 +1,11 @@
-<?php
-echo '<h2>'.$news_item['title'].'</h2>';
-echo '<p>'.$news_item['text'].'</p>';
-echo '<p>Name: '.$news_item['name'].'</p>';
-echo '<p>Nickname: '.$news_item['nickname'].'</p>';
-echo '<p>E-mail: '.$news_item['email'].'</p>';
-echo '<p>Address: '.$news_item['address'].'</p>';
-echo '<p>Gender: '.$news_item['gender'].'</p>';
-echo '<p>Cellphone Number: '.$news_item['cellno'].'</p>';
-echo '<p>Comments: '.$news_item['comments'].'</p>'; ?>
+<div class = "jumbotron text-center">
+<?php echo '<h1>'.$news_item['title'].'</h1>';?>
+<?php echo '<p>'.$news_item['text'].'</p>';?>
+</div>
+<?php echo '<p>Name: '.$news_item['name'].'</p>';?>
+<?php echo '<p>Nickname: '.$news_item['nickname'].'</p>';?>
+<?php echo '<p>E-mail: '.$news_item['email'].'</p>';?>
+<?php echo '<p>Address: '.$news_item['address'].'</p>';?>
+<?php echo '<p>Gender: '.$news_item['gender'].'</p>';?>
+<?php echo '<p>Cellphone Number: '.$news_item['cellno'].'</p>';?>
+<?php echo '<p>Comments: '.$news_item['comments'].'</p>';?>
