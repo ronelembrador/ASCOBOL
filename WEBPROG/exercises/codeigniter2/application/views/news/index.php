7919ffdfb1dfa32bf6cac19e4888f401e1e968d0
ronelembrador <omembrador@student.apc.edu.ph>
2017-04-07 10:54:12 +0800

    Changing various words into glyphicons and changing of action links to button group

--- WEBPROG/exercises/codeigniter2/application/views/news/index.php
+++ WEBPROG/exercises/codeigniter2/application/views/news/index.php
@@ -2,32 +2,34 @@
  
 <table class = "table-bordered table-hover" border='1' cellpadding='4'>
     <tr>
-        <td><strong>Title</strong></td>
-        <td><strong>Content</strong></td>
-        <td><strong>Name</strong></td>
-        <td><strong>Nickname</strong></td>
-        <td><strong>E-mail</strong></td>
-        <td><strong>Address</strong></td>
-        <td><strong>Gender</strong></td>
-        <td><strong>Cellphone #</strong></td>
-        <td><strong>Comments</strong></td>
-        <td><strong>Action</strong></td>
+        <td class = "text-center well-sm"><strong>Title</strong></td>
+        <td class = "text-center well-sm"><strong>Content</strong></td>
+        <td class = "text-center well-sm"><strong>Name</strong></td>
+        <td class = "text-center well-sm"><strong>Nickname</strong></td>
+        <td class = "text-center well-sm"><span class="glyphicon glyphicon-envelope"></span></td>
+        <td class = "text-center well-sm"><span class="glyphicon glyphicon-home"></span></td>
+        <td class = "text-center well-sm"><strong>Gender</strong></td>
+        <td class = "text-center well-sm"><span class="glyphicon glyphicon-phone"></span></td>
+        <td class = "text-center well-sm"><strong>Comments</strong></td>
+        <td class = "text-center well-sm"><strong>Action</strong></td>
     </tr>
 <?php foreach ($news as $news_item): ?>
         <tr>
-            <td><?php echo $news_item['title']; ?></td>
-            <td><?php echo $news_item['text']; ?></td>
-            <td><?php echo $news_item['name']; ?></td>
-            <td><?php echo $news_item['nickname']; ?></td>
-            <td><?php echo $news_item['email']; ?></td>
-            <td><?php echo $news_item['address']; ?></td>
-            <td><?php echo $news_item['gender']; ?></td>
-            <td><?php echo $news_item['cellno']; ?></td>
-            <td><?php echo $news_item['comments']; ?></td>
-            <td>
-                <a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View</a> |
-                <a href="<?php echo site_url('news/edit/'.$news_item['id']); ?>">Edit</a> |
-                <a href="<?php echo site_url('news/delete/'.$news_item['id']); ?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
+            <td class="well-sm"><?php echo $news_item['title']; ?></td>
+            <td class="well-sm"><?php echo $news_item['text']; ?></td>
+            <td class="well-sm"><?php echo $news_item['name']; ?></td>
+            <td class="well-sm"><?php echo $news_item['nickname']; ?></td>
+            <td class="well-sm"><?php echo $news_item['email']; ?></td>
+            <td class="well-sm"><?php echo $news_item['address']; ?></td>
+            <td class="well-sm"><?php echo $news_item['gender']; ?></td>
+            <td class="well-sm"><?php echo $news_item['cellno']; ?></td>
+            <td class="well-sm"><?php echo $news_item['comments']; ?></td>
+            <td class="well-sm">
+                <div class="btn-group btn-group-justified">
+                <a href="<?php echo site_url('news/'.$news_item['slug']); ?>" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open" style="color:silver"></span></a>
+                <a href="<?php echo site_url('news/edit/'.$news_item['id']); ?> " class="btn btn-primary"><span class ="glyphicon glyphicon-edit" style="color:silver"></span></a>
+                <a href="<?php echo site_url('news/delete/'.$news_item['id']); ?>" onClick="return confirm('Are you sure you want to delete?')" class="btn btn-primary"><span class =" glyphicon glyphicon-remove" style="color:red"></span></a>
+                </div>
             </td>
         </tr>
 <?php endforeach; ?>
--- WEBPROG/exercises/codeigniter2/application/views/templates/footer.php
+++ WEBPROG/exercises/codeigniter2/application/views/templates/footer.php
@@ -1,4 +1,7 @@
-<p><em>Copyright © 2016</em></p>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>" type="text/javascript"></script>
+<p><em>Copyright <span class="glyphicon glyphicon-copyright-mark"></span> 2016</em></p>
 		</div>
         </body>
+
 </html>
